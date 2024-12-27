68c6d7ed0f5749aa6860612a72caed135b1d99ad
Appolo-13 <rayl1x.gta@gmail.com>
2024-12-27 12:06:46 +0100

    Added conversion from Fahrenheit to Celsius.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+    private static final double COEFFICIENT = 5.0 / 9.0;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("This project will convert celsius to fahrenheit and vice versa.");
+
+        double fahr = 77.1;
+        double cels = 23.7;
+        double convertedFahToCel = convertedFahToCel(fahr);
+        System.out.println("Fahrenheits without conversion to celsius: " + fahr + "\n" +
+                "Fahrenheits with conversion to celsius: " + convertedFahToCel);
+
+    }
+    private static double convertedFahToCel(double fahr) {
+        return (fahr - 32) * COEFFICIENT;
     }
 }
